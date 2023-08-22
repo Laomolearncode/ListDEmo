693241f388c533ec128794e4c015e348723c506e
Laomo <3066475098@qq.com>
2023-08-22 21:07:05 +0800

    modify tese

--- Dictdemo/demo1.py
+++ Dictdemo/demo1.py
@@ -5,9 +5,11 @@ print(price)
 print(scores)
 #字典元素的获取
 print(price['耳机'])
-print(socres.get('高数'))
+print(scores.get('高数'))
 '''
    使用[]获取不存在的键值会报错，
    而使用get()方法获取不存在的键值会返回none，
    如果get()里面带参数，则会返回默认值参数
 '''
+b=sorted(price.items(),key=lambda price:price[1])
+print(b)
